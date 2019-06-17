f44726a16d1dbfa4f30a4cb5c85ff9b186747f95
Hasunie <hasunie@wso2.com>
2019-06-17 16:58:48 +0530

    adding error message

--- components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/mediators/XMLSchemaValidator.java
+++ components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/mediators/XMLSchemaValidator.java
@@ -127,7 +127,7 @@ public class XMLSchemaValidator extends AbstractMediator {
             AnalyzerHolder.returnObject(apimThreatAnalyzer);
         } else {
             if (logger.isDebugEnabled()) {
-                logger.debug("XML Schema Validator: " + APIMgtGatewayConstants.REQUEST_TYPE_FAIL_MSG);
+                logger.debug("XML Schema Validator: Neither request method nor content type is matched with");
             }
         }
         GatewayUtils.setOriginalInputStream(inputStreams, axis2MC);
